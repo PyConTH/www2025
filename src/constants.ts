4cd0b095d9f84d1f9ebbb56407e44fd0bb8f7dd3
Pee Tankulrat <8246751+PiiTan@users.noreply.github.com>
2025-02-23 11:21:54 +0700

    Also, add Social media icons to the Hero section

---
+++ src/constants.ts
@@ -0,0 +1,35 @@
+export const socials = [
+  {
+    icon: "logos:discord-icon",
+    link: "https://discord.gg/Hg7cUDzvwu",
+  },
+  {
+    icon: "devicon:google",
+    link: "https://www.google.co.th/search?q=pycon+thailand",
+  },
+  {
+    icon: "logos:messenger",
+    link: "http://m.me/PyConThailand",
+  },
+  {
+    icon: "ri:line-fill",
+    link: "",
+    color: "#00b900",
+  },
+  {
+    icon: "logos:twitter",
+    link: "https://twitter.com/pyconthailand",
+  },
+  {
+    icon: "mdi:github",
+    link: "",
+  },
+  {
+    icon: "logos:microsoft-teams",
+    link: "",
+  },
+  {
+    icon: "logos:facebook",
+    link: "https://www.facebook.com/PyConThailand",
+  },
+];

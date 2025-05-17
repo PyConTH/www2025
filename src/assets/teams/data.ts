b4ea6848c9875f12f673f37a837622cb6a88e34b
Pee Tankulrat <8246751+PiiTan@users.noreply.github.com>
2025-05-17 18:48:42 +0700

    Our Team section

---
+++ src/assets/teams/data.ts
@@ -0,0 +1,116 @@
+import IceImage from "@assets/teams/images/ice.jpg";
+import JamesImage from "@assets/teams/images/james.jpeg";
+import JirachaiImage from "@assets/teams/images/jirachai.jpg";
+import KaewImage from "@assets/teams/images/kaew.png";
+import NatworpongImage from "@assets/teams/images/natworpong.jpg";
+import NuttakitImage from "@assets/teams/images/nuttakit.jpg";
+import PanthipaImage from "@assets/teams/images/panthipa.jpg";
+import PeeImage from "@assets/teams/images/pee.jpeg";
+import PhantipImage from "@assets/teams/images/phantip.jpg";
+import PhatchanyaImage from "@assets/teams/images/phatchanya.jpg";
+import PhattaradaImage from "@assets/teams/images/phattarada.png";
+import PitiImage from "@assets/teams/images/piti.jpeg";
+import SiriwimonImage from "@assets/teams/images/siriwimon-profile.jpeg";
+import SudaratImage from "@assets/teams/images/sudarat.jpeg";
+
+export const TeamData = [
+  {
+    name: "Phantip Kokilanon",
+    role: "Co-chair",
+    image: PhantipImage,
+    title: "Vice President at Thai Programmer Association",
+    desc: "Microsoft Certified Trainer, community manager at Spark Tech Thailand, co-chair of PyCon Thailand, educational writer at CAMPHUB, Vice President at Thai Programmer Association, Deputy Secretary General at Thai Webmaster and Online Media Association, and former intern at Microsoft who like technology that makes the world a more beautiful place.",
+  },
+  {
+    name: "Sudarat Chattanon",
+    role: "Co-chair",
+    image: SudaratImage,
+    title: "Lead Software Engineer",
+    desc: "Gatuk is co-chair of PyCon Thailand 2025, a driving force behind PyLadies Bangkok, and a lead software engineer at Jitta. She see coding as an art, loves growing communities, and making tech more welcoming for everyone.",
+  },
+  {
+    name: "Kanin Kearpimy",
+    role: "Co-chair",
+    image: JamesImage,
+    title: "PhD Student at The University of Melbourne",
+    desc: "Kanin is general practitioner of PyCon Thailand. While he is PhD student at the University of Melbourne.",
+  },
+  {
+    name: "Manussanun Buranachokphaisan",
+    role: "CFP Commitee",
+    image: KaewImage,
+    title: "Lead Data & AI at Investic",
+    desc: "",
+  },
+  {
+    name: "Piti Champeethong",
+    role: "CFP commitee.",
+    image: PitiImage,
+    title: "Senior consulting engineer at MongoDB Singapore",
+    desc: "Piti is a CFP committee of PyCon Thailand. At the same time, he is a Microsoft MVP in Developer Technologies.\nPlease connect with him at https://www.linkedin.com/in/pitichampeethong/",
+  },
+  {
+    name: "Ice Peeranat",
+    role: "Core Team",
+    image: IceImage,
+    title: "Senior Associate Platform Engineer at Arise by Infinitas",
+    desc: "Just a man diving to see the bottom of the iceberg, both in technology and in life.",
+  },
+  {
+    name: "Natworpong Loyswai",
+    role: "Logistic",
+    image: NatworpongImage,
+    title:
+      "Chief Technoloy Officer at SHIPPUSU CO., LTD., Cybersecurity Consultant at CYNIUS Cybertech and Consulting",
+    desc: "Natworpong is the Chief Technoloy Officer at SHIPPUSU CO., LTD. and a Cybersecurity Consultant at CYNIUS Cybertech and Consulting.",
+  },
+  {
+    name: "Panthipa Suksirisorn",
+    role: "MarCom (PR)",
+    image: PanthipaImage,
+    title: "Associate Consultant, WE Tech Consulting",
+    desc: "Panthipa is PR of PyCon Thailand. While she passionate about using psychology, innovation, and community building to inspire growth.",
+  },
+  {
+    name: "Nuttakit Kundum",
+    role: "MarCom (PR)",
+    image: NuttakitImage,
+    title: "Blockchain Developer",
+    desc: "Nuttakit Kundum (F) is on the Public Relations team of PyCon Thailand. While he maintains open-source projects under the zksolutions organization and contributes to the Ream Beam chain.",
+  },
+  {
+    name: "Phattrada Mikota",
+    role: "MarCom (PR)",
+    image: PhattaradaImage,
+    title: "Senior Software Engineering Student at Thammasat University",
+    desc: "",
+  },
+  {
+    name: "Bumbim, Siriwimon",
+    role: "Sponsorship",
+    image: SiriwimonImage,
+    title: "Senior Quality Analyst at Thoughtworks",
+    desc: "",
+  },
+  {
+    name: "Pee Tankulrat",
+    role: "Tech",
+    image: PeeImage,
+    title: "Team Lead @ Edsy",
+    desc: "I try my best to make the world a better place through tech.",
+  },
+  {
+    name: "Phatchanya Chongsheveewat",
+    role: "Tech",
+    image: PhatchanyaImage,
+    title: "Software Engineer in Test at LINE Pay",
+    desc: "",
+  },
+  {
+    name: "Jirachai Chansivanon",
+    role: "Tech",
+    image: JirachaiImage,
+    title: "",
+    desc: "",
+  },
+];

f1696555b1034da443ad7f9cd85589f4e68e7b1b
kanin-kearpimy <kanin.kearpimy@gmail.com>
2025-07-26 15:35:21 +1000

    Added CfP Reviewer

---
+++ src/assets/teams/cfp-reviewer.ts
@@ -0,0 +1,21 @@
+import KaewImage from "@assets/teams/images/kaew.png";
+import LutfiImage from "@assets/teams/images/lutfi.jpeg";
+import PitiImage from "@assets/teams/images/piti.jpeg";
+
+export const CfPReviewerTeamData = [
+  {
+    name: "Lutfi Zuchri",
+    image: LutfiImage,
+    title: "Commitee Chair at PyCon APAC 2024",
+  },
+  {
+    name: "Piti Champeethong",
+    image: PitiImage,
+    title: "Senior consulting engineer at MongoDB Singapore",
+  },
+  {
+    name: "Manussanun Buranachokphaisan",
+    image: KaewImage,
+    title: "Lead Data & AI at Investic",
+  },
+];
